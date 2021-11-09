791a8709596c6241c6beb64bcd9322377bb89302
Daniel Krupiński <danielkrupinski@outlook.com>
2021-11-09 23:03:32 +0100

    Implement example use of the merging constructor

---
+++ Examples/MergingConstructor.cpp
@@ -0,0 +1,26 @@
+#include <cassert>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "StringPool.h"
+
+int main()
+{
+    StringPool<char> pool1, pool2, pool3, pool4;
+
+    const std::string_view s1 = pool1.add("first string");
+    const std::string_view s2 = pool2.add("second string");
+    const std::string_view s3 = pool3.add("third string");
+    const std::string_view s4 = pool4.add("fourth string");
+
+    // Merging constructor can be used to combine multiple StringPool objects into one
+    // It is useful when multiple pools are filled across threads then the results can be stored in a single object
+    StringPool<char> allInOne{ std::move(pool1), std::move(pool2), std::move(pool3), std::move(pool4) };
+
+    // Strings stored in any of the initial pools remain valid
+    assert(s1 == "first string");
+    assert(s2 == "second string");
+    assert(s3 == "third string");
+    assert(s4 == "fourth string");
+}
