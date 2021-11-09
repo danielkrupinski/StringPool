9144ec9313e696bc000bdba2c289f84bb6185c61
Daniel Krupiński <danielkrupinski@outlook.com>
2021-11-09 23:05:26 +0100

    Add [[maybe_unused]] attribute to variables used only in debug mode

--- Examples/MergingConstructor.cpp
+++ Examples/MergingConstructor.cpp
@@ -9,10 +9,10 @@ int main()
 {
     StringPool<char> pool1, pool2, pool3, pool4;
 
-    const std::string_view s1 = pool1.add("first string");
-    const std::string_view s2 = pool2.add("second string");
-    const std::string_view s3 = pool3.add("third string");
-    const std::string_view s4 = pool4.add("fourth string");
+    [[maybe_unused]] const std::string_view s1 = pool1.add("first string");
+    [[maybe_unused]] const std::string_view s2 = pool2.add("second string");
+    [[maybe_unused]] const std::string_view s3 = pool3.add("third string");
+    [[maybe_unused]] const std::string_view s4 = pool4.add("fourth string");
 
     // Merging constructor can be used to combine multiple StringPool objects into one
     // It is useful when multiple pools are filled across threads then the results can be stored in a single object
