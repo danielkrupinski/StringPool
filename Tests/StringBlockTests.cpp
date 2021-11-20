f17ed50783f42083d188aae253e61f42df923970
Daniel Krupiński <danielkrupinski@outlook.com>
2021-11-20 10:12:31 +0100

    Access fields of a test fixture through 'this'

--- Tests/StringBlockTests.cpp
+++ Tests/StringBlockTests.cpp
@@ -35,92 +35,92 @@ using TypesToTest = testing::Types<
 TYPED_TEST_CASE(StringBlockOfZeroCapacity, TypesToTest);
 
 TYPED_TEST(StringBlockOfZeroCapacity, HasNoFreeSpace) {
-    ASSERT_EQ(block.getFreeSpace(), 0);
+    ASSERT_EQ(this->block.getFreeSpace(), 0);
 }
 
 TYPED_TEST(StringBlockOfZeroCapacity, CannotTakeStringOfNonzeroLength) {
-    ASSERT_FALSE(block.canTakeStringOfLength(100));
+    ASSERT_FALSE(this->block.canTakeStringOfLength(100));
 }
 
 TYPED_TEST(StringBlockOfZeroCapacity, CanTakeStringOfZeroLengthIfNotNullterminating) {
-    ASSERT_EQ(block.canTakeStringOfLength(0), TypeParam::getSpaceRequiredToStoreStringOfLength(0) == 0);
+    ASSERT_EQ(this->block.canTakeStringOfLength(0), TypeParam::getSpaceRequiredToStoreStringOfLength(0) == 0);
 }
 
 TYPED_TEST(StringBlockOfZeroCapacity, AddedEmptyStringHasZeroLength) {
-    if (block.canTakeStringOfLength(0))
-        ASSERT_EQ(block.addString({}).length(), 0);
+    if (this->block.canTakeStringOfLength(0))
+        ASSERT_EQ(this->block.addString({}).length(), 0);
 }
 
 TYPED_TEST_CASE(StringBlockOfCapacityOfOne, TypesToTest);
 
 TYPED_TEST(StringBlockOfCapacityOfOne, HasFreeSpaceForOneChar) {
-    ASSERT_EQ(block.getFreeSpace(), 1);
+    ASSERT_EQ(this->block.getFreeSpace(), 1);
 }
 
 TYPED_TEST(StringBlockOfCapacityOfOne, CanTakeEmptyString) {
-    ASSERT_TRUE(block.canTakeStringOfLength(0));
+    ASSERT_TRUE(this->block.canTakeStringOfLength(0));
 }
 
 TYPED_TEST(StringBlockOfCapacityOfOne, CanTakeStringOfLengthOfOneIfNotNullterminating) {
-    ASSERT_EQ(block.canTakeStringOfLength(1), TypeParam::getSpaceRequiredToStoreStringOfLength(1) == 1);
+    ASSERT_EQ(this->block.canTakeStringOfLength(1), TypeParam::getSpaceRequiredToStoreStringOfLength(1) == 1);
 }
 
 TYPED_TEST(StringBlockOfCapacityOfOne, CannotTakeStringLongerThanOneChar) {
-    ASSERT_FALSE(block.canTakeStringOfLength(123));
+    ASSERT_FALSE(this->block.canTakeStringOfLength(123));
 }
 
 TYPED_TEST(StringBlockOfCapacityOfOne, AddedEmptyStringHasZeroLength) {
-    ASSERT_EQ(block.addString({}).length(), 0);
+    ASSERT_EQ(this->block.addString({}).length(), 0);
 }
 
 TYPED_TEST(StringBlockOfCapacityOfOne, AddedStringPreservesLength) {
-    if (block.canTakeStringOfLength(1))
-        ASSERT_EQ(block.addString(std::basic_string<TypeParam::StringType::value_type>(1, '7')).length(), 1);
+    if (this->block.canTakeStringOfLength(1))
+        ASSERT_EQ(this->block.addString(std::basic_string<TypeParam::StringType::value_type>(1, '7')).length(), 1);
 }
 
 TYPED_TEST(StringBlockOfCapacityOfOne, AddedStringHasDifferentMemoryLocation) {
-    if (block.canTakeStringOfLength(1)) {
+    if (this->block.canTakeStringOfLength(1)) {
         const std::basic_string<TypeParam::StringType::value_type> toAdd(1, '7');
-        ASSERT_NE(block.addString(toAdd).data(), toAdd.data());
+        ASSERT_NE(this->block.addString(toAdd).data(), toAdd.data());
     }
 }
 
 TYPED_TEST_CASE(StringBlockOfNonzeroCapacity, TypesToTest);
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, FreeSpaceEqualsCapacityWhenEmpty) {
-    ASSERT_EQ(block.getFreeSpace(), capacity);
+    ASSERT_EQ(this->block.getFreeSpace(), this->capacity);
 }
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, CanTakeEmptyString) {
-    ASSERT_TRUE(block.canTakeStringOfLength(0));
+    ASSERT_TRUE(this->block.canTakeStringOfLength(0));
 }
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, CanTakeStringOfLengthEqualingCapacityIfNotNullterminating) {
-    ASSERT_EQ(block.canTakeStringOfLength(capacity), TypeParam::getSpaceRequiredToStoreStringOfLength(capacity) == capacity);
+    ASSERT_EQ(this->block.canTakeStringOfLength(this->capacity), TypeParam::getSpaceRequiredToStoreStringOfLength(this->capacity) == this->capacity);
 }
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, CanTakeStringOfShorterThanCapacity) {
-    ASSERT_TRUE(block.canTakeStringOfLength(capacity - 1));
+    ASSERT_TRUE(this->block.canTakeStringOfLength(this->capacity - 1));
 }
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, CannotTakeStringLongerThanCapacity) {
-    ASSERT_FALSE(block.canTakeStringOfLength(capacity + 1));
+    ASSERT_FALSE(this->block.canTakeStringOfLength(this->capacity + 1));
 }
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, AddedEmptyStringHasZeroLength) {
-    ASSERT_EQ(block.addString({}).length(), 0);
+    ASSERT_EQ(this->block.addString({}).length(), 0);
 }
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, AddedStringPreservesLength) {
-    ASSERT_EQ(block.addString(std::basic_string<TypeParam::StringType::value_type>(capacity - 1, 'a')).length(), capacity - 1);
+    ASSERT_EQ(this->block.addString(std::basic_string<TypeParam::StringType::value_type>(this->capacity - 1, 'a')).length(), this->capacity - 1);
 }
 
 TYPED_TEST(StringBlockOfNonzeroCapacity, AddedStringHasDifferentMemoryLocation) {
-    const std::basic_string<TypeParam::StringType::value_type> toAdd(capacity - 1, 'a');
-    ASSERT_NE(block.addString(toAdd).data(), toAdd.data());
+    const std::basic_string<TypeParam::StringType::value_type> toAdd(this->capacity - 1, 'a');
+    ASSERT_NE(this->block.addString(toAdd).data(), toAdd.data());
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
